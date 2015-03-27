2ab05025129fb6307967ba83ee4f2c4d7513cbcf
Dan Wright <muscle.crab.13@gmail.com>
2015-03-26 22:37:49 -0700

    add revisions so specs pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,57 @@
-var triangleType = function(un, deux, trois) {
+var triangleType = function(sideOne, sideTwo, sideThree) {
 
-  var sideAry = [un, deux, trois]
+  var equalSidesCount = 0;
+  var triangleSides = [sideOne, sideTwo, sideThree];
 
-  var equalCount = 0;
+  triangleSides.sort(function (b, a) {
+    return a - b;
+  });
 
-  if (sideAry[0] > (sideAry[1] + sideAry[2])) {
+  if (triangleSides[0] > (triangleSides[1] + triangleSides[2])) {
     return "invalid"
   }
 
-  else if ((un === deux) && (deux === trois)) {
+  else if ((sideOne === sideTwo) && (sideTwo === sideThree)) {
     return "equilateral"
-
   }
 
   else {
       for (var i = 0; i < 2; i++) {
 
-      sideAry.forEach(function(side) {
+      triangleSides.forEach(function(side) {
 
-        if ((side === sideAry[i]) && (sideAry.indexOf(side) !== i) && (equalCount === 0)) {
-          equalCount++;
+        if ((side === triangleSides[i]) && (triangleSides.indexOf(side) !== i) && (equalSidesCount === 0)) {
+          equalSidesCount++;
         }
 
       })
     }
-    if (equalCount === 1) {
+
+    if (equalSidesCount === 1) {
       return "isosceles"
 
     } else {
       return "scalene"
-
     }
-
-
   }
-
-
 }
-
-
 
 $(document).ready(function() {
   $("form#sideForm").submit(function(event) {
 
-
-    var sideA = parseInt($("input#sideA").val());
-    var sideB = parseInt($("input#sideB").val());
-    var sideC = parseInt($("input#sideC").val());
-
     var sides = [sideA, sideB, sideC]
-
-    sides = sides.sort(function (a, b) {
-      return b - a;
-    });
-
-
 
     if ((sideA < 1 ) || (sideB < 1 ) || (sideC < 1 )) {
       alert("What you are doing is illegal and we won't allow it.");
       return false;
     }
 
-    var result = triangleType(sideA, sideB, sideC);
+    var result = triangleType(sides[0], sides[1], sides[2]);
 
     $("#results").empty();
 
-
     $("#results").append(result);
-    
+
     event.preventDefault();
   });
 });
